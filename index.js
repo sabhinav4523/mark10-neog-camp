0126e1d5026fe6493ce5fcc406533c13a361b911
ABHINAV KUMAR SINGH <b216049@iiit-bh.ac.in>
2021-09-22 16:52:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+//fetching the input from html to javascript
+const billAmount=document.querySelector("#bill-amount");
+const checkButton= console.log(billAmount.value);
+//checking the bill amount
+//const cashGiven = document.querySelector("#cash-given")
+//const checkButton = document.querySelector("#check-button");
+//console.log(cashGiven.value);
